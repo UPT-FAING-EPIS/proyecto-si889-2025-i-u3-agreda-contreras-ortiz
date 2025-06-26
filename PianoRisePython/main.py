6c06bbc6dd14a3da976add8cf70233c3e2d99836
ximena-ortiz <xo2021071080@virtual.upt.pe>
2025-06-26 11:06:56 -0500

    [@ximena-ortiz] Sentry

--- PianoRisePython/main.py
+++ PianoRisePython/main.py
@@ -4,8 +4,33 @@ monkey.patch_all()
 import os
 from app import create_app, socketio
 
+from flask import request
+
+import sentry_sdk
+sentry_url = os.getenv("SENTRY_URL", "")
+sentry_sdk.init(
+    dsn=sentry_url,
+    # Add data like request headers and IP for users,
+    # see https://docs.sentry.io/platforms/python/data-management/data-collected/ for more info
+    send_default_pii=True,
+    traces_sample_rate=1.0,
+)
+
+
 app = create_app()
 port = int(os.environ.get("PORT", 5000))
+
+@app.before_request
+def log_request_to_sentry():
+    sentry_sdk.capture_message(
+        f"{request.method} {request.path}",
+        level="info"
+    )
+
+@app.route("/error")
+def hello_world():
+    1/0  # raises an error
+    return "<p>Hello, World!</p>"
 
 environment = os.getenv("ENVIRONMENT", "development")
 
@@ -16,5 +41,3 @@ if __name__ == "__main__":
     else:
         print(f"Servidor corriendo en http://127.0.0.1:{port}")
         socketio.run(app, host="127.0.0.1", port=port, debug=True)
-
-
